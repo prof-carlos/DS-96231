30a576378379472009cc9599f41d0a9a1cf886b6
WAS <wlsa291273@outlook.com>
2026-02-11 21:24:48 -0300

    salvar

--- JavaBasico/src/main/java/org/example/Main.java
+++ JavaBasico/src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner ler = new Scanner(System.in);
+        String loginSalvo = "Marta";
+        String senhaSalva = "123";
+        String login;
+        String senha;
+
+        do {
+            System.out.println("Digite seu login: ");
+            login = ler.nextLine();
+
+            System.out.println("Digite sua senha: ");
+            senha = ler.nextLine();
+
+            if (login.equals(loginSalvo) && senha.equals(senhaSalva)) {
+                System.out.println("Bem-vindo, usuário logado!");
+            } else {
+                System.out.println("Nome de usuário ou senha inválidos.");
+            }
+        } while (!login.equals(loginSalvo) || !senha.equals(senhaSalva));
     }
 }
